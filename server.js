83a9b6088faa281648a2f17f5e7bd83933b9cc85
grilledcheeseplease <lacynunnally402@gmail.com>
2022-06-22 19:57:09 -0400

    trying to work on error can't get /notes

--- server.js
+++ server.js
@@ -9,59 +9,59 @@ const PORT = process.env.PORT || 3001;
 
 // Add data parsing
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Had tutor help
 // Set up routes 
 app.get('public/notes', (req, res) => {
     fs.readFile('db/db.json', 'utf-8', (err, data) => {
-        if(err) throw err;
+        if (err) throw err;
         const notes = JSON.parse(data);
-
-        // API routes
-        app.get('/api/notes', (req, res) => {
-            res.json(notes);
-        });
-
-        app.post('/api/notes', (req, res) => {
-            const newNote = req.body;
-            notes.push(newNote);
-            dbTracker();
-            return console.log('Note added ' + newNote.title);
-        });
-
-        // When we want to get a note at a specific id
-        app.get('/api/notes/:id', (req, res) => {
-            res.json(notes[req.params.id]);
-        });
-
-        // Where we are going to delete notes 
-        app.delete('/api/notes/:id', (req, res) => {
-            notes.splice(req.params.id, 1);
-            dbTracker();
-            console.log('Note deleted ' + req.params.id);
-        });
-
-        // Set up views to display html files 
-        app.get('/notes', (req, res) => {
-            res.sendFile(path.join(__dirname, '/public/notes.html'));
-        });
-
-        app.get('*', (req, res) => {
-            res.sendFile(path.join(__dirname, '/public/index.html'));
-        });
-
-        // Function to track notes added and deleted 
-        const dbTracker = () => {
-            fs.writeFile('db/db.json', JSON.stringify(notes, '\t'), err => {
-                if(err) throw err;
-                return true;
-            });
-        };
-
     });
 });
+
+// API routes
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+    notes.push(newNote);
+    dbTracker();
+    return console.log('Note added ' + newNote.title);
+});
+
+// When we want to get a note at a specific id
+app.get('/api/notes/:id', (req, res) => {
+    res.json(notes[req.params.id]);
+});
+
+// Where we are going to delete notes 
+app.delete('/api/notes/:id', (req, res) => {
+    notes.splice(req.params.id, 1);
+    dbTracker();
+    console.log('Note deleted ' + req.params.id);
+});
+
+// Set up views to display html files 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+// Function to track notes added and deleted 
+const dbTracker = () => {
+    fs.writeFile('db/db.json', JSON.stringify(notes, '\t'), err => {
+        if (err) throw err;
+        return true;
+    });
+};
+
 
 // Listener goes at the bottom
 app.listen(PORT, () => {
