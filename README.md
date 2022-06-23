fa82fcadeb676cdde0c8e2d1306a4e5cd63c4bac
grilledcheeseplease <lacynunnally402@gmail.com>
2022-06-22 23:25:51 -0400

    the css file needed an s... so i rewrote this code just to find that style.css needed to be styles.css

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Note-Taker
+
+## Description
+
+This is a note taking application that will allow you to, you guessed it, write and save notes. There's a landing page with a link that will take you to the notes page. When you enter a new note title a save icon pops up in the navigation bar to save your note once it's typed up. 
+
+When you save a note it will show up under the note tutle on the left sidebar. If you want to view it again just click it. You can use the plus icon to get new empty field to write your note in. Once you are done with a note and wish to remove it you can click the trashcan and delete the note. 
+
+## Table of Contents 
+- [Description](#description)
+- [Visuals](#visuals)
+- [Resources](#resources)
+
+## Visuals
+
+[Note Taker]()
+
+## Resources
+
+- [Repository](https://github.com/grilledcheeseplease/Note-Taker)
--- server.js
+++ server.js
@@ -1,40 +1,27 @@
-// Bring in node packages
+// Pull in needed mods and files
 const express = require('express');
-const path = require('path');
 const fs = require('fs');
-const noteData = require('./db/db.json');
+const path = require('path');
+const noteDb = require('./db/db.json');
 
-// Set up variable for express() function and port
+// Create variable for express function and PORT
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Add data parsing
+// Parsing data
+app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
 
-// API routes
+// GET functions for pathing and APIs
 app.get('/api/notes', (req, res) => {
-    res.json(noteData.slice(1));
+    res.json(noteDb.slice(1));
 });
 
-app.post('/api/notes', (req, res) => {
-    const newNote = addNote(req.body, noteData);
-    res.json(addNote);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-// When we want to get a note at a specific id
-app.get('/api/notes/:id', (req, res) => {
-    res.json(notes[req.params.id]);
-});
-
-// Where we are going to delete notes 
-app.delete('/api/notes/:id', (req, res) => {
-    dbTracker(req.params.id, noteData);
-    res.json(true);
-});
-
-// Set up views to display html files 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
@@ -44,37 +31,60 @@ app.get('*', (req, res) => {
 });
 
 // Function to add notes 
-const addNote = (body, noteArray) => {
+const addNote = (body, noteData) => {
     const newNote = body;
-    if(!Array.isArray(noteArray))
-    noteArray = [];
 
-    if(noteArray.length === 0)
-    noteArray[0]++;
+    if (!Array.isArray(noteData))
+        noteData = [];
+    
+    if (noteData.length === 0)
+        noteData.push(0);
 
-    noteArray.push(newNote);
+    body.id = noteData[0];
+    noteData[0]++;
+
+    noteData.push(newNote);
+
     fs.writeFileSync(
-        path.join(__dirname, './db/dbljson'),
-        JSON.stringify(noteArray, null, 2)
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(noteData, null, 2)
     );
-    return newNote;
-}
 
-// Function to track notes added and deleted 
-const dbTracker = (id, noteArray) => {
-    for(let i = 0; i < notes.length; i++) {
-        if (notes[i].id == id) {
-            notes.splice(i, 1);
-            //short circuit the loop, since there will only be one match on ID
+    return newNote;
+};
+
+// Post where addNote function will be called 
+app.post('/api/notes', (req, res) => {
+    const newNote = addNote(req.body, noteDb);
+    res.json(newNote);
+});
+
+// Function to delete notes 
+const deleteNote = (id, noteData) => {
+    for (let i = 0; i < noteData.length; i++) {
+        let note = noteData[i];
+
+        if (note.id == id) {
+            noteData.splice(i, 1);
+
+            fs.writeFileSync(
+                path.join(__dirname, './db/db.json'),
+                JSON.stringify(noteData, null, 2)
+            );
+
             break;
         }
     }
-
 };
 
+// Delete where deleteNote function will be called 
+app.delete('/api/notes/:id', (req, res) => {
+    deleteNote(req.params.id, noteDb);
+    res.json(true);
+});
 
-// Listener goes at the bottom
+// Listeners go at the bottom and listen for connections
 app.listen(PORT, () => {
-    console.log(`App listening at port ${PORT}`);
+    console.log(`API server now on port ${PORT}!`);
 });
 
